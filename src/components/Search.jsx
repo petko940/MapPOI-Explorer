7bf58a2964c4821642ff904f2da73eb648146191
Petko Todorov <p.todorov1015@gmail.com>
2024-09-09 14:50:05 +0300

    small change (will be edited again)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ const Search = ({ setCenter, setMarkers }) => {
 
     const handleSearch = async (e) => {
         e.preventDefault();
-        
+
         try {
             const response = await fetch(
                 `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(location)}&format=json`
@@ -53,32 +53,39 @@ const Search = ({ setCenter, setMarkers }) => {
                     const maxDiff = Math.max(latDiff, lonDiff);
 
                     if (maxDiff < 0.05) {
-                        zoomLevel += 2; 
+                        zoomLevel += 2;
                     } else if (maxDiff < 0.1) {
-                        zoomLevel += 1; 
+                        zoomLevel += 1;
                     } else if (maxDiff > 2) {
-                        zoomLevel -= 1;                     }
+                        zoomLevel -= 1;
+                    }
                 }
 
                 setCenter({ lat: parseFloat(lat), lon: parseFloat(lon), zoom: zoomLevel });
                 setMarkers([{ lat: parseFloat(lat), lon: parseFloat(lon), name: location }]);
             } else {
-                alert('Location not found in Bulgaria');
+                alert('Location not found');
             }
         } catch (error) {
-            console.error('Error fetching location data:', error);
+            console.error(error);
         }
     };
 
     return (
-        <form onSubmit={handleSearch}>
+        <form onSubmit={handleSearch} className="flex justify-start">
             <input
                 type="text"
                 value={location}
                 onChange={(e) => setLocation(e.target.value)}
                 placeholder="Enter a location"
+                className="w-1/5 p-2 border border-gray-300 rounded-sm"
             />
-            <button type="submit">Search</button>
+            <button
+                type="submit"
+                className="ml-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300"
+            >
+                Search
+            </button>
         </form>
     );
 };
