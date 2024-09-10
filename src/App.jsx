f8c1cb38c7dc988c1c471d9b483b6a65571ef006
Petko Todorov <p.todorov1015@gmail.com>
2024-09-10 15:18:07 +0300

    add points of interest

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import background from './assets/background.jpg';
 function App() {
     const [center, setCenter] = useState({ lat: 42.743, lon: 25.62, zoom: 7.5 });
     const [markers, setMarkers] = useState([]);
+    const [pois, setPOIs] = useState([]);
 
     return (
         <div className='h-screen' style={{ backgroundImage: `url(${background})` }}>
@@ -15,14 +16,20 @@ function App() {
 
             <div className='flex'>
                 <div className="w-9/12 rounded-sm mx-5">
-                    <Map center={center} markers={markers} setCenter={setCenter} setMarkers={setMarkers} />
+                    <Map
+                        center={center}
+                        markers={markers}
+                        setCenter={setCenter}
+                        setMarkers={setMarkers}
+                        pois={pois}
+                        setPOIs={setPOIs}
+                    />
                 </div>
 
                 <div>
                     Text
                 </div>
             </div>
-
 
         </div>
     );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,19 +2,30 @@ import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from 're
 import 'leaflet/dist/leaflet.css';
 import RecentMap from './RecentMap.jsx';
 import MapClickHandler from './MapClickHandler';
+import PointsOfInterest from './PointsOfInterest.jsx';
 
-const Map = ({ center, markers, setCenter, setMarkers }) => {
+const Map = ({ center, markers, setCenter, setMarkers, pois, setPOIs }) => {
     return (
-        <MapContainer center={[center.lat, center.lon]} zoom={center.zoom} style={{ height: '70vh'}}>
+        <MapContainer center={[center.lat, center.lon]} zoom={center.zoom} style={{ height: '70vh' }}>
             <RecentMap center={center} />
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <MapClickHandler setCenter={setCenter} setMarkers={setMarkers} /> {/* Handle map clicks */}
+
+            <MapClickHandler setCenter={setCenter} setMarkers={setMarkers} />
+
             {markers.map((marker, idx) => (
                 <Marker key={idx} position={[marker.lat, marker.lon]}>
                     <Popup>{marker.name}</Popup>
+                </Marker>
+            ))}
+
+            <PointsOfInterest center={center} setPOIs={setPOIs} /> {/* Load POIs based on the map center */}
+
+            {pois.map((poi, idx) => (
+                <Marker key={idx} position={[poi.lat, poi.lon]}>
+                    <Popup>{poi.name}</Popup>
                 </Marker>
             ))}
         </MapContainer>
