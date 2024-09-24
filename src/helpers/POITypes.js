443fd4ac2a4d63b2213ee0cd6ff2372f0f15688b
Petko Todorov <p.todorov1015@gmail.com>
2024-09-24 15:23:30 +0300

    Initial commit

---
+++ src/helpers/POITypes.js
@@ -0,0 +1,22 @@
+const POITypes = {
+    amenity: [
+        'restaurant', 'cafe', 'hospital', 'parking', 'pharmacy', 'bank', 'police', 'fuel'
+    ],
+    shop: [
+        'supermarket', 'convenience', 'bakery', 'clothes', 'electronics', 'pharmacy'
+    ],
+    tourism: [
+        'hotel', 'museum', 'zoo', 'viewpoint'
+    ],
+    leisure: [
+        'park', 'playground', 'stadium', 'swimming_pool'
+    ],
+    landuse: [
+        'residential', 'commercial', 'industrial'
+    ],
+    natural: [
+        'water', 'beach', 'forest'
+    ],    
+};
+
+export default POITypes;
