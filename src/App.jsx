513fcd14466a51b920c97b55c90bc4aa744ae77d
Petko Todorov <p.todorov1015@gmail.com>
2024-09-24 15:22:23 +0300

    add POI Type Selector

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import Map from './components/Map';
 import Search from './components/Search';
 import background from './assets/background.jpg';
 
+import PointsOfInterest from './components/PointsOfInterest';
+import POITypeSelector from './components/POITypeSelector';  // The dropdown for selecting POI type
+
 function App() {
     const [center, setCenter] = useState({ lat: 42.743, lon: 25.62, zoom: 7.5 });
     const [markers, setMarkers] = useState([]);
     const [pois, setPOIs] = useState([]);
+    const [selectedType, setSelectedType] = useState("");
+    
+    const handleTypeChange = (type) => {
+        setSelectedType(type);
+
+        if (!type) {
+            setPOIs([]);
+        };
+    };
 
     return (
         <div className='h-screen' style={{ backgroundImage: `url(${background})` }}>
+            {markers[0] && <POITypeSelector onTypeChange={handleTypeChange} />}
+
             <div className='p-5'>
                 <Search setCenter={setCenter} setMarkers={setMarkers} />
             </div>
@@ -30,8 +44,10 @@ function App() {
                     Text
                 </div>
             </div>
+            <PointsOfInterest center={center} setPOIs={setPOIs} selectedType={selectedType} />
 
         </div>
+
     );
 };
 
