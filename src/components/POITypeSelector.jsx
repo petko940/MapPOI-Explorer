ea5dacac29caa1722d7a49fb27fedb278ff956d6
Petko Todorov <p.todorov1015@gmail.com>
2024-10-28 15:25:54 +0200

    major change on type selector buttons

--- src/components/POITypeSelector.jsx
+++ src/components/POITypeSelector.jsx
@@ -7,7 +7,6 @@ const POITypeSelector = ({
     selectedMainCategory,
     setSelectedMainCategory,
 }) => {
-    // const [selectedMainCategory, setSelectedMainCategory] = useState(null);
     const [selectedSubCategory, setSelectedSubCategory] = useState(null);
 
     const handleMainCategoryClick = (category) => {
@@ -34,50 +33,37 @@ const POITypeSelector = ({
 
     return (
         <div>
-            {!selectedMainCategory && (
-                <div className="flex flex-wrap space-x-2 mb-4">
-                    {Object.keys(POITypes).map((category) => (
-                        <button
-                            key={category}
-                            onClick={() => handleMainCategoryClick(category)}
-                            className={`p-2 border rounded mb-2 text-white capitalize ${selectedMainCategory === category
-                                ? 'bg-green-500'
-                                : 'bg-blue-500'
-                                }`}
-                            disabled={markers.length === 0}
-                        >
-                            {category}
-                        </button>
-                    ))}
-                </div>
-            )}
+            <div className="flex flex-wrap justify-center space-x-2">
+                {Object.keys(POITypes).map((category) => (
+                    <button
+                        key={category}
+                        onClick={() => handleMainCategoryClick(category)}
+                        className={`p-2 border rounded mb-2 text-white capitalize ${markers.length === 0
+                            ? 'bg-gray-400 cursor-not-allowed'
+                            : selectedMainCategory === category
+                                ? 'bg-green-500' : selectedMainCategory
+                                    ? 'bg-gray-400'
+                                    : 'bg-blue-500'
+                            }`}
+                        disabled={markers.length === 0}
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
 
             {selectedMainCategory && (
-                <div>
-                    <div className="mb-4">
+                <div className="flex flex-wrap justify-center space-x-2">
+                    {POITypes[selectedMainCategory].map((subCategory) => (
                         <button
-                            onClick={() => handleMainCategoryClick(selectedMainCategory)}
-                            className="p-2 border rounded bg-green-500 text-white mb-2 capitalize"
+                            key={subCategory}
+                            onClick={() => handleSubCategoryClick(subCategory)}
+                            className={`p-2 border rounded mb-2 text-white capitalize ${selectedSubCategory === subCategory ? 'bg-green-500' : 'bg-blue-500'
+                                }`}
                         >
-                            {selectedMainCategory}
+                            {subCategory}
                         </button>
-                    </div>
-
-                    <div className="flex flex-wrap space-x-2">
-                        {POITypes[selectedMainCategory].map((subCategory) => (
-                            <button
-                                key={subCategory}
-                                onClick={() => handleSubCategoryClick(subCategory)}
-                                className={`p-2 border rounded mb-2 text-white capitalize ${selectedSubCategory === subCategory
-                                    ? 'bg-green-500'
-                                    : 'bg-blue-500'
-                                    }`}
-                                disabled={markers.length === 0}
-                            >
-                                {subCategory}
-                            </button>
-                        ))}
-                    </div>
+                    ))}
                 </div>
             )}
         </div>
@@ -85,4 +71,3 @@ const POITypeSelector = ({
 };
 
 export default POITypeSelector;
-
