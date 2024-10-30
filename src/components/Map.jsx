0ef7274d4b63d60cc184d8bfa6ff446d5bd3f582
Petko Todorov <p.todorov1015@gmail.com>
2024-10-30 14:33:06 +0200

    change some default markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,44 @@ import RecentMap from './RecentMap.jsx';
 import MapClickHandler from './MapClickHandler';
 import PointsOfInterest from './PointsOfInterest.jsx';
 
+
+import {
+    defaultIcon,
+    restaurantIcon,
+    coffeeIcon,
+    hospitalIcon,
+    parkingIcon,
+    pharmacyIcon,
+    bankIcon,
+    policeStationIcon,
+    gasStationIcon,
+} from '../helpers/mapIcons';
+
+
 const Map = ({ center, markers, setCenter, setMarkers, pois, setPOIs }) => {
+    const getIconForType = (type) => {
+        switch (type) {
+            case 'restaurant':
+                return restaurantIcon;
+            case 'cafe':
+                return coffeeIcon;
+            case 'hospital':
+                return hospitalIcon;
+            case 'parking':
+                return parkingIcon;
+            case 'pharmacy':
+                return pharmacyIcon;
+            case 'bank':
+                return bankIcon;
+            case 'police':
+                return policeStationIcon;
+            case 'fuel':
+                return gasStationIcon;
+            default:
+                return defaultIcon;
+        };
+    };
+
     return (
         <MapContainer center={[center.lat, center.lon]} zoom={center.zoom} style={{ height: '70vh' }}>
             <RecentMap center={center} />
@@ -16,15 +53,15 @@ const Map = ({ center, markers, setCenter, setMarkers, pois, setPOIs }) => {
             <MapClickHandler setCenter={setCenter} setMarkers={setMarkers} />
 
             {markers.map((marker, idx) => (
-                <Marker key={idx} position={[marker.lat, marker.lon]}>
+                <Marker key={idx} position={[marker.lat, marker.lon]} icon={getIconForType(marker.type)}>
                     <Popup>{marker.name}</Popup>
                 </Marker>
             ))}
 
-            <PointsOfInterest center={center} setPOIs={setPOIs} /> 
+            <PointsOfInterest center={center} setPOIs={setPOIs} />
 
             {pois.map((poi, idx) => (
-                <Marker key={idx} position={[poi.lat, poi.lon]}>
+                <Marker key={idx} position={[poi.lat, poi.lon]} icon={getIconForType(poi.type)}>
                     <Popup>{poi.name}</Popup>
                 </Marker>
             ))}
