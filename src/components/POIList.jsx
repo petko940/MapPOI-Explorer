bf99167ed892cae3382c4afc07ba1c7f814c29b9
Petko Todorov <p.todorov1015@gmail.com>
2024-11-25 15:20:30 +0200

    add css

--- src/components/POIList.jsx
+++ src/components/POIList.jsx
@@ -1,8 +1,19 @@
-const POIList = ({ pois, handlePOIClick }) => {
+const POIList = ({ pois, handlePOIClick, selectedPOI }) => {
+    const isSelected = (poi) => {
+        return selectedPOI && 
+            Math.abs(selectedPOI.lat - poi.lat) < 0.0001 && 
+            Math.abs(selectedPOI.lon - poi.lon) < 0.0001;
+    };
+
     return (
-        <div>
+        <div className="overflow-y-auto h-[70vh]">
             {pois.map((poi, idx) => (
-                <div key={idx} onClick={() => handlePOIClick(poi)}>
+                <div
+                    key={idx}
+                    onClick={() => handlePOIClick(poi)}
+                    className={`p-2 border-b border-gray-300 cursor-pointer select-none 
+                        ${isSelected(poi) ? 'bg-blue-100' : 'hover:bg-gray-100'}`}
+                >
                     <p>{poi.name}</p>
                 </div>
             ))}
